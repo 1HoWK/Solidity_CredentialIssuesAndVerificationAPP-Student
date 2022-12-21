e544b47e749458d18b0ec93ade9ff1678cdbf6cb
varManWai <laizoke98@gmail.com>
2022-12-22 00:47:13 +0800

    added the header and meta and ico"

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 import "../styles/globals.css";
 import StudentLayout from "../components/Layouts/layout";
 import { SessionProvider } from "next-auth/react";
-
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <StudentLayout>
-        <Component {...pageProps} />
-      </StudentLayout>
-    </SessionProvider>
+    <>
+      <SessionProvider session={pageProps.session}>
+        <Head>
+          <meta charset="UTF-8" />
+          <meta name="keywords" content="Certificate, badge, blockchain" />
+          <meta name="author" content="Lai & Ho" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>CredBLOCK</title>
+          <meta
+            name="description"
+            content="A Application for the credential issues and verification"
+          />
+          <link rel="icon" href="/images/ico.svg" />
+        </Head>
+        <StudentLayout>
+          <Component {...pageProps} />
+        </StudentLayout>
+      </SessionProvider>
+    </>
   );
 }
